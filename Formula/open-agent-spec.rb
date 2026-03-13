514ee1c049ed8a4736bf722c4a395b3be1926278
Andrew whitehouse <andrewswhitehouse@gmail.com>
2026-03-13 20:41:14 +1100

    feat fix:

--- Formula/open-agent-spec.rb
+++ Formula/open-agent-spec.rb
@@ -1,4 +1,6 @@
 class OpenAgentSpec < Formula
+  include Language::Python::Virtualenv
+
   desc "CLI for creating and running Open Agent Spec projects"
   homepage "https://www.openagentstack.ai"
   url "https://files.pythonhosted.org/packages/6e/cc/db2861662d4f40262b535554a87c94d00424d24a1c43f9c5bb81d7cd1ea0/open_agent_spec-1.2.4.tar.gz"
@@ -8,9 +10,7 @@ class OpenAgentSpec < Formula
   depends_on "python@3.12"
 
   def install
-    system "python3", "-m", "pip", "install", "hatchling"
-    system "python3", "-m", "pip", "install", "--no-build-isolation", *std_pip_args, "."
-    generate_completions_from_executable(bin/"oa", shell_parameter_format: :typer)
+    virtualenv_install_with_resources
   end
 
   test do
