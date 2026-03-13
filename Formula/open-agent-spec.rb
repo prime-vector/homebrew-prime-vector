6c174d03cf51c679de1d20a27f2e08f67da8b8c6
Andrew whitehouse <andrewswhitehouse@gmail.com>
2026-03-13 20:22:59 +1100

    feat: update version

--- Formula/open-agent-spec.rb
+++ Formula/open-agent-spec.rb
@@ -1,7 +1,7 @@
 class OpenAgentSpec < Formula
   desc "CLI for creating and running Open Agent Spec projects"
   homepage "https://www.openagentstack.ai"
-  url "https://files.pythonhosted.org/packages/6b/65/10e01dc2a02393d30ae764f4251f588cbf633a5636e8563664d9511225a9b0ab/open_agent_spec-1.2.4.tar.gz"
+  url "https://files.pythonhosted.org/packages/6e/cc/db2861662d4f40262b535554a87c94d00424d24a1c43f9c5bb81d7cd1ea0/open_agent_spec-1.2.4.tar.gz"
   sha256 "10e01dc2a02393d30ae764f4251f588cbf633a5636e8563664d9511225a9b0ab"
   license "MIT"
 
