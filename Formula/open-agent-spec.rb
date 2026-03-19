935cc96543d29775038696dec08eb2bf2da36b5f
Andrew whitehouse <andrewswhitehouse@gmail.com>
2026-03-19 13:59:04 +1100

    feat: bump version

--- Formula/open-agent-spec.rb
+++ Formula/open-agent-spec.rb
@@ -1,8 +1,8 @@
 class OpenAgentSpec < Formula
   desc "CLI for creating and running Open Agent Spec projects"
   homepage "https://www.openagentstack.ai"
-  url "https://files.pythonhosted.org/packages/ed/26/6e61d09c8cc0b1af4d6f4e1cc493573d7143fd856ca64b207aaed1ebc596/open_agent_spec-1.2.5.tar.gz"
-  sha256 "da741ffebf2cab3efce13358a56186c593cc72a01aa84b20478dc45f6515f8c0"
+  url "https://files.pythonhosted.org/packages/75/56/1ff55f746c0449f5eba0c7ca2edbba54c6577e6b0fe13e9d61999d0fa4fa/open_agent_spec-1.2.7.tar.gz"
+  sha256 "b0de50440e2f53968d05615173090ab311490a67f36eb9c1c8b3eea9b6275ea7"
   license "MIT"
 
   depends_on "python@3.12"
